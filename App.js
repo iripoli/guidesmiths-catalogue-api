0e26d74c8865824b3a585b707b688c5f4721ac65
iripoli <iripoli96@gmail.com>
2020-07-16 14:35:00 +0200

    routes config done, with an error handler for wrong routes

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ const morgan = require('morgan')
 const path = require('path')
 const cors = require('cors');
 
+const indexRouter = require('./routes/index.routes')
+const phonesRouter = require('./routes/phones.routes')
+
 
 const app = express()
 
@@ -16,5 +19,22 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
+
+
+//ROUTES
+//app.use('/api/phones', phonesRouter)
+app.use('/api', indexRouter)
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.status(err.status || 500).json(err.message)
+})
 
 app.listen(PORT, console.log(`Server is listening on port ${PORT}`));
